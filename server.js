fe8111a2213dbe7e9eaeb2a18f7d65a2a794e99d
thierno thiam <thiernothiam@esp.sn>
2017-02-22 11:16:01 +0000

    subscribe socket

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 var _ = require('lodash');
 var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
@@ -48,10 +50,18 @@ app.get("/salon",function (req,res) {
     });
 });
 
+io.on('connection', function (socket) {
+    socket.on("subscribe",function (user) {
+        user.socket = socket.id;
+        discussions.push(user);
+        socket.emit('good', {code: 200});
+    });
+});
+
 
 let port = process.env.PORT || 8080;
 
 
-let server = app.listen(port, function () {
+server.listen(port, function () {
     console.log('Server running at http://127.0.0.1:'+port);
 });
